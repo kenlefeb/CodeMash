bfc3f50a18ad581be19edc93247db619ac973301
Kenneth LeFebvre <kenneth@lefebvre.us>
2012-10-18 18:03:20 -0400

    Empty Store Grid App
    We renamed it and began to separate some of the generated classes into
    their own files.

---
+++ src/Spakll.CodeMash/Spakll.CodeMash/DataModel/Sessions/SampleDataItem.cs
@@ -0,0 +1,43 @@
+using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using Windows.ApplicationModel.Resources.Core;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Imaging;
+using System.Collections.Specialized;
+
+namespace Spakll.CodeMash.Data
+{
+    /// <summary>
+    /// Generic item data model.
+    /// </summary>
+    public class SampleDataItem : SampleDataCommon
+    {
+        public SampleDataItem(String uniqueId, String title, String subtitle, String imagePath, String description, String content, SampleDataGroup group)
+            : base(uniqueId, title, subtitle, imagePath, description)
+        {
+            this._content = content;
+            this._group = group;
+        }
+
+        private string _content = string.Empty;
+        public string Content
+        {
+            get { return this._content; }
+            set { this.SetProperty(ref this._content, value); }
+        }
+
+        private SampleDataGroup _group;
+        public SampleDataGroup Group
+        {
+            get { return this._group; }
+            set { this.SetProperty(ref this._group, value); }
+        }
+    }
+}
